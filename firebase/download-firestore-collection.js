056d71dfeb6bd38cbc225843bd9957a46043676b
DESKTOP-TDTN8OG\Ruben <rubenruano@gmail.com>
2022-04-04 21:37:30 -0300

    commit 4

--- firebase/download-firestore-collection.js
+++ firebase/download-firestore-collection.js
@@ -21,24 +21,24 @@ import {
 } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js';
 import { writeJsonSync } from 'https://deno.land/x/jsonfile/mod.ts';
 
-
-// Read firebase config
-const configFile = './env.json';
-const firebaseConfig = JSON.parse(Deno.readTextFileSync(configFile));
-
-if(!firebaseConfig) {
-    console.error(`Could not read firebase config from ${configFile}`);
-    Deno.exit(1);
-}
-
 // Parsear los argumentos de la línea de comandos
 const args = parse(Deno.args);
 const collectionName = args.collection;
+const configFile = args.config || './env.json';
 
 if (!collectionName) {
   console.log('No collection name provided');
   Deno.exit(1);
 }
+
+// Read firebase config
+const firebaseConfig = JSON.parse(Deno.readTextFileSync(configFile));
+if(!firebaseConfig) {
+    console.error(`Could not read firebase config from ${configFile}`);
+    Deno.exit(1);
+}
+
+console.log(firebaseConfig);
 
 // Inicializar la aplicación de Firebase
 const app = initializeApp(firebaseConfig);
@@ -48,8 +48,6 @@ const db = getFirestore(app);
 const docs = [];
 const querySnapshot = await getDocs(collection(db, collectionName));
 querySnapshot.forEach((doc) => {
-  // doc.data() is never undefined for query doc snapshots
-  console.log(doc.id, " => ", doc.data());
   docs.push({id: doc.id, ...doc.data()});
 });
 
