b964a2a3072fc5a2455ea317802288d035153bdd
ruanosoftware <66127879+ruanosoftware@users.noreply.github.com>
2022-06-03 11:15:29 -0300

    Create personas.js

---
+++ graphql/personas.js
@@ -0,0 +1,102 @@
+export const personas = [
+  {
+    id: 1,
+    first_name: 'Astrid',
+    last_name: 'Wiseman',
+    email: 'awiseman0@creativecommons.org',
+    gender: 'Female',
+    ip_address: '231.212.207.186',
+    street: 'Street A',
+    city: 'City C'
+  },
+  {
+    id: 2,
+    first_name: 'Gene',
+    last_name: 'Seifenmacher',
+    email: 'gseifenmacher1@mapy.cz',
+    gender: 'Female',
+    ip_address: '30.41.154.241',
+    street: 'Street A',
+    city: 'City C'
+  },
+  {
+    id: 3,
+    first_name: 'Emili',
+    last_name: 'Pleace',
+    email: 'epleace2@wikimedia.org',
+    gender: 'Female',
+    ip_address: '0.166.36.239',
+    street: 'Street A',
+    city: 'City C'
+  },
+  {
+    id: 4,
+    first_name: 'Merrick',
+    last_name: 'Anfossi',
+    email: 'manfossi3@miibeian.gov.cn',
+    gender: 'Male',
+    ip_address: '59.47.247.91',
+    street: 'Street A',
+    city: 'City C'
+  },
+  {
+    id: 5,
+    first_name: 'Sharai',
+    last_name: 'de Castelain',
+    email: 'sdecastelain4@wordpress.com',
+    gender: 'Female',
+    ip_address: '139.212.227.88',
+    street: 'Street A',
+    city: 'City C'
+  },
+  {
+    id: 6,
+    first_name: 'Laina',
+    last_name: 'Leimster',
+    email: 'lleimster5@mediafire.com',
+    gender: 'Non-binary',
+    ip_address: '128.254.8.31',
+    street: 'Street A',
+    city: 'City C'
+  },
+  {
+    id: 7,
+    first_name: 'Ferris',
+    last_name: 'Khristoforov',
+    email: 'fkhristoforov6@github.com',
+    gender: 'Male',
+    ip_address: '124.16.245.127',
+    street: 'Street A',
+    city: 'City C'
+  },
+  {
+    id: 8,
+    first_name: 'Marcello',
+    last_name: 'Brimblecomb',
+    email: 'mbrimblecomb7@sitemeter.com',
+    gender: 'Male',
+    ip_address: '129.151.61.180',
+    street: 'Street A',
+    city: 'City C'
+  },
+  {
+    id: 9,
+    first_name: 'Katy',
+    last_name: 'Kuschel',
+    email: 'kkuschel8@nhs.uk',
+    gender: 'Female',
+    ip_address: '166.231.152.94',
+    street: 'Street A',
+    city: 'City C'
+  },
+  {
+    id: 10,
+    first_name: 'Leah',
+    last_name: 'Mash',
+    email: 'lmash9@reddit.com',
+    gender: 'Female',
+    ip_address: '149.179.124.84',
+    street: 'Street A',
+    city: 'City C'
+  },
+];
